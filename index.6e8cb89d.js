9a3eff590c9bba0ef170893b33c1fa4a9741a89f
Jonathan Meza <jduplantier44@gmail.com>
2022-06-28 00:03:36 -0500

    Updates

--- index.6e8cb89d.js
+++ index.6e8cb89d.js
@@ -46,17 +46,49 @@ const l=function(){const r=document.createElement("link").relList;if(r&&r.suppor
     </div>
     `}}customElements.define("power-puff-eye",o);class s extends HTMLElement{constructor(){super(),this.attachShadow({mode:"open"})}static get styles(){return`
       :host {
-        --width: 50px;
-        --height: 20px;
+        --width: 30px;
+        --height: 30px;
 
         width: var(--width);
         height: var(--height);
-        display: flex;
         background: #fff;
+        box-shadow: 0 8px 0 #0005 inset;
+        display: flex;
+        border: 3px solid #000;
+        position: relative;
+        top: -3px;
       }
 
       :host(.up){
+        height: 90%;
+        box-shadow: none;
+        border-radius:50%;
+        transform: translate(-5%, -40%);
+      }
 
+
+      .shoe {
+        width: 120%;
+        height: 100%;
+        background: #000;
+        border-radius: 0 0 50% 50%;
+        position: absolute;
+        transform: translate(-10%, 75%);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      .sock {
+        background: #fff;
+        width: 45%;
+        height: 40%;
+        border-radius: 0 0 50% 50%;
+        clip-path: polygon(0 25%, 100% 25%, 100% 100%, 0 100%);
+      }
+
+      :host(.up) .shoe {
+        transform: translate(-10%, 25%);
       }
     `}connectedCallback(){this.render()}render(){this.shadowRoot.innerHTML=`
     <style>${s.styles}</style>
@@ -131,6 +163,8 @@ const l=function(){const r=document.createElement("link").relList;if(r&&r.suppor
           drop-shadow(0 -2px 0 #000),
           drop-shadow(0 2px 0 #000);
         transform: translateY(-5%);
+        z-index: 2;
+        position: relative;
       }
       .body-container .body {
         width: 100%;
@@ -143,6 +177,11 @@ const l=function(){const r=document.createElement("link").relList;if(r&&r.suppor
             #000 28% 71%,
             var(--power-puff-color) 60% 100%
           );
+      }
+
+      .legs {
+        display: flex;
+        z-index: 5;
       }
     `}connectedCallback(){this.render()}render(){this.shadowRoot.innerHTML=`
     <style>${a.styles}</style>
